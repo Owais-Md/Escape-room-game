360dd9bd4ad4d2a000c27f9202dee4c7e5cc2c15
Owais-Md <mohammad.owais@iiitb.ac.in>
2023-08-07 20:12:13 +0530

    Drawing by the layers

--- Background.lua
+++ Background.lua
@@ -7,7 +7,8 @@ function Background:New(scale, world)
     local background = {}
     setmetatable(background, self)
     self.__index = self
-    background.map = sti('Maps/Map 1.lua')
+    --background.map = sti('Maps/Map 1.lua')
+    background.map = sti('Tiled/Room 1.lua')
     background.walls = {}
     if background.map.layers["Game Walls"] then
         for i, obj in pairs(background.map.layers["Game Walls"].objects)do
@@ -18,10 +19,20 @@ function Background:New(scale, world)
     end
     return background
 end
-
-
-function Background:Draw()
-    self.map:draw(0, 0, scale)
+--[[
+function Background:drawLayer(layer)
+    background.map:drawLayer(layer)
+end
+]]
+function Background:Draw()love.graphics.push() -- Save the current transformation state
+    love.graphics.scale(5) -- Scale the drawing by a factor of 5
+    for _, layer in ipairs(self.map.layers) do
+        if layer.name ~= "Game Walls" then
+            self.map:drawLayer(layer)
+        end
+    end
+    love.graphics.pop() -- Restore the previous transformation state
+    --self.map:draw(0, 0, scale)
 end
 
 return Background
