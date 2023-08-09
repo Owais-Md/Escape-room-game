5359758e395e156bdd5a3466613e489cda285c0d
Owais-Md <mohammad.owais@iiitb.ac.in>
2023-08-09 18:23:15 +0530

    Tileset information stored here

---
+++ basictiles.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.2",
+  name = "basictiles",
+  class = "",
+  tilewidth = 16,
+  tileheight = 16,
+  spacing = 0,
+  margin = 0,
+  columns = 8,
+  image = "Sprites/Lanea Zimmerman's spritesheets/basictiles.png",
+  imagewidth = 128,
+  imageheight = 240,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 120,
+  tiles = {}
+}
---
+++ things.lua
@@ -0,0 +1,344 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.2",
+  name = "things",
+  class = "",
+  tilewidth = 16,
+  tileheight = 16,
+  spacing = 0,
+  margin = 0,
+  columns = 12,
+  image = "Sprites/Lanea Zimmerman's spritesheets/things.png",
+  imagewidth = 192,
+  imageheight = 128,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 96,
+  tiles = {
+    {
+      id = 1,
+      animation = {
+        {
+          tileid = 1,
+          duration = 100
+        },
+        {
+          tileid = 13,
+          duration = 100
+        },
+        {
+          tileid = 25,
+          duration = 100
+        },
+        {
+          tileid = 37,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 2,
+      animation = {
+        {
+          tileid = 38,
+          duration = 100
+        },
+        {
+          tileid = 26,
+          duration = 100
+        },
+        {
+          tileid = 14,
+          duration = 100
+        },
+        {
+          tileid = 2,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 4,
+      animation = {
+        {
+          tileid = 4,
+          duration = 100
+        },
+        {
+          tileid = 16,
+          duration = 100
+        },
+        {
+          tileid = 28,
+          duration = 100
+        },
+        {
+          tileid = 40,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 5,
+      animation = {
+        {
+          tileid = 41,
+          duration = 100
+        },
+        {
+          tileid = 29,
+          duration = 100
+        },
+        {
+          tileid = 17,
+          duration = 100
+        },
+        {
+          tileid = 5,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 7,
+      animation = {
+        {
+          tileid = 7,
+          duration = 100
+        },
+        {
+          tileid = 19,
+          duration = 100
+        },
+        {
+          tileid = 31,
+          duration = 100
+        },
+        {
+          tileid = 43,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 8,
+      animation = {
+        {
+          tileid = 44,
+          duration = 100
+        },
+        {
+          tileid = 32,
+          duration = 100
+        },
+        {
+          tileid = 20,
+          duration = 100
+        },
+        {
+          tileid = 8,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 10,
+      animation = {
+        {
+          tileid = 10,
+          duration = 100
+        },
+        {
+          tileid = 22,
+          duration = 100
+        },
+        {
+          tileid = 34,
+          duration = 100
+        },
+        {
+          tileid = 46,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 48,
+      animation = {
+        {
+          tileid = 48,
+          duration = 100
+        },
+        {
+          tileid = 49,
+          duration = 100
+        },
+        {
+          tileid = 50,
+          duration = 100
+        },
+        {
+          tileid = 49,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 60,
+      animation = {
+        {
+          tileid = 60,
+          duration = 100
+        },
+        {
+          tileid = 61,
+          duration = 100
+        },
+        {
+          tileid = 62,
+          duration = 100
+        },
+        {
+          tileid = 61,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 72,
+      animation = {
+        {
+          tileid = 72,
+          duration = 100
+        },
+        {
+          tileid = 73,
+          duration = 100
+        },
+        {
+          tileid = 74,
+          duration = 100
+        },
+        {
+          tileid = 73,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 81,
+      animation = {
+        {
+          tileid = 81,
+          duration = 100
+        },
+        {
+          tileid = 82,
+          duration = 100
+        },
+        {
+          tileid = 83,
+          duration = 100
+        },
+        {
+          tileid = 82,
+          duration = 100
+        },
+        {
+          tileid = 69,
+          duration = 100
+        },
+        {
+          tileid = 70,
+          duration = 100
+        },
+        {
+          tileid = 71,
+          duration = 100
+        },
+        {
+          tileid = 70,
+          duration = 100
+        },
+        {
+          tileid = 57,
+          duration = 100
+        },
+        {
+          tileid = 58,
+          duration = 100
+        },
+        {
+          tileid = 59,
+          duration = 100
+        },
+        {
+          tileid = 58,
+          duration = 100
+        },
+        {
+          tileid = 57,
+          duration = 100
+        },
+        {
+          tileid = 71,
+          duration = 100
+        },
+        {
+          tileid = 70,
+          duration = 100
+        },
+        {
+          tileid = 69,
+          duration = 100
+        },
+        {
+          tileid = 83,
+          duration = 100
+        },
+        {
+          tileid = 82,
+          duration = 100
+        },
+        {
+          tileid = 81,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 84,
+      animation = {
+        {
+          tileid = 84,
+          duration = 100
+        },
+        {
+          tileid = 85,
+          duration = 100
+        },
+        {
+          tileid = 86,
+          duration = 100
+        },
+        {
+          tileid = 85,
+          duration = 100
+        }
+      }
+    }
+  }
+}
